2832a955a395ca1cba9de7f6fbb7fb4788fa3a7a
dahee <118436137+daheenamic@users.noreply.github.com>
2025-09-02 19:23:53 +0900

    [Bronze V] Title: 합, Time: 108 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for (int i = 1; i <= a; i++) {
+            result += i;
+        }
+
+        System.out.println(result);
+    }
+}
