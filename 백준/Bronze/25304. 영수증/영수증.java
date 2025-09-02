f5663ac20520f102148fbd2ed5ae9975b968cc5e
dahee <118436137+daheenamic@users.noreply.github.com>
2025-09-02 19:27:21 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0; i < b; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int c = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            sum += (c * d);
+        }
+
+        if(a == sum) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
