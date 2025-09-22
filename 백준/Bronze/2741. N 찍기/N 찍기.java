03b97a18c84653d09415d2f43ef27fae22281ce3
dahee <118436137+daheenamic@users.noreply.github.com>
2025-09-22 21:13:55 +0900

    [Bronze V] Title: N 찍기, Time: 188 ms, Memory: 18136 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= a; i++) {
+            sb.append(i).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
