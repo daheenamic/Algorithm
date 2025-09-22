f6bcce0a1d87dbedb3087e63cbefa4ecdd547c80
dahee <118436137+daheenamic@users.noreply.github.com>
2025-09-22 22:28:13 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 116 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        for (int i = n - 1; i > 0 ; i--) {
+            for (int j = 0; j < n - i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
