f72c00f74751dc26fa349edffcd93af107b49f33
dahee <118436137+daheenamic@users.noreply.github.com>
2025-10-23 21:48:12 +0900

    [Bronze II] Title: 단어의 개수, Time: 240 ms, Memory: 20164 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -4,14 +4,11 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine().trim();
-
-        if (line.isEmpty()) {
-            System.out.println(0);
-            return;
-        }
-
-        StringTokenizer st = new StringTokenizer(line, " ");
+        
+        String input = br.readLine();
+        
+        StringTokenizer st = new StringTokenizer(input);
+        
         System.out.println(st.countTokens());
     }
 }
