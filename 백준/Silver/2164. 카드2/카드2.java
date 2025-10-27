a4bcda568407328c35f41b721f3116797520672a
dahee <118436137+daheenamic@users.noreply.github.com>
2025-10-27 20:40:39 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 45572 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.addLast(i);
+        }
+
+        while (q.size() > 1) {
+            q.removeFirst();
+            q.addLast(q.removeFirst());
+        }
+
+        System.out.println(q.peek());
+    }
+}
