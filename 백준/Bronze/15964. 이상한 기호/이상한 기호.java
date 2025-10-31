d86ab769e8938f6639afca336f6e3d0decf2d5f7
dahee <118436137+daheenamic@users.noreply.github.com>
2025-10-31 17:27:00 +0900

    [Bronze V] Title: 이상한 기호, Time: 100 ms, Memory: 14324 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -3,15 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        System.out.println(calculator(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        long a =  Long.parseLong(st.nextToken());
+        long b =  Long.parseLong(st.nextToken());
+        System.out.println(calculator(a, b));
     }
 
-    public static int calculator(int a, int b) {
+    public static long calculator(long a, long b) {
         return (a + b) * (a - b);
     }
 }
