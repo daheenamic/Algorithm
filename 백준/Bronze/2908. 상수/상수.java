71427e8ef72c254bb8b28cd773acfbdafe7107ab
dahee <118436137+daheenamic@users.noreply.github.com>
2025-10-31 17:49:19 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -8,24 +8,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] arr1 = st.nextToken().split("");
-        String[] arr2 = st.nextToken().split("");
-
-        String a = "";
-        String b = "";
-
-        for (int i = arr1.length - 1; i >= 0; i--) {
-            a += arr1[i];
-        }
-
-        for (int i = arr2.length - 1; i >= 0; i--) {
-            b += arr2[i];
-        }
+        String a = new StringBuilder(st.nextToken()).reverse().toString();
+        String b = new StringBuilder(st.nextToken()).reverse().toString();
 
         int n = Integer.parseInt(a);
         int m = Integer.parseInt(b);
 
-        if (n > m) System.out.println(n);
-        else System.out.println(m);
+        System.out.println(Math.max(n, m));
     }
 }
