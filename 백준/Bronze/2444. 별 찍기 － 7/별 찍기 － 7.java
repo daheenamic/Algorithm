5b573a510e69c667cd88197ea46eff937ee2d7a5
dahee <118436137+daheenamic@users.noreply.github.com>
2025-11-10 22:09:09 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -6,37 +6,13 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        int m = 2 * n - 1;
-        int star = 1;
-        int space = 1;
 
         for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n-i; j++) {
-                sb.append(" ");
-            }
-
-            for(int k = 1; k <= star; k++) {
-                sb.append("*");
-            }
-
-            star += 2;
-            sb.append("\n");
+            sb.append(" ".repeat(n-i)).append("*".repeat(2*i-1)).append("\n");
         }
 
-        star -= 2;
-
         for(int i = n-1; i >= 1; i--) {
-            for(int j = 1; j <= space; j++) {
-                sb.append(" ");
-            }
-
-            for(int k = 1; k <= star - 2; k++) {
-                sb.append("*");
-            }
-            star -= 2;
-            space++;
-
-            sb.append("\n");
+            sb.append(" ".repeat(n-i)).append("*".repeat(2*i-1)).append("\n");
         }
 
         System.out.println(sb);
