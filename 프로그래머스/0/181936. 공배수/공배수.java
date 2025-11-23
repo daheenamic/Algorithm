72f6afb1c7158a6e121deaa5dcd1d88a1744638d
dahee <118436137+daheenamic@users.noreply.github.com>
2025-11-23 20:29:30 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,20 +1,5 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        int answer = 0;
-        
-        if (number % n == 0 && number % m == 0) {
-            answer = 1;
-        }
-        
-        return answer;
-    }
-    
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        int number = 60;
-        int n = 2;
-        int m = 3;
-        
-        System.out.println(s.solution(number, n, m));
+        return number % n == 0 && number % m == 0 ? 1 : 0;
     }
 }
