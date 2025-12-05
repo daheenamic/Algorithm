f555f9b7e021b3d68829cb771449619caef0316a
dahee <118436137+daheenamic@users.noreply.github.com>
2025-12-05 23:34:19 +0900

    [Silver V] Title: 너의 평점은, Time: 108 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,54 +1,40 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
+        Map<String, Double> gradeMap = new HashMap<>();
+        gradeMap.put("A+", 4.5);
+        gradeMap.put("A0", 4.0);
+        gradeMap.put("B+", 3.5);
+        gradeMap.put("B0", 3.0);
+        gradeMap.put("C+", 2.5);
+        gradeMap.put("C0", 2.0);
+        gradeMap.put("D+", 1.5);
+        gradeMap.put("D0", 1.0);
+        gradeMap.put("F", 0.0);
+
         double totalScore = 0;
         double sum = 0;
 
-        while ((line = br.readLine()) != null) {
-            StringTokenizer st = new StringTokenizer(line);
-            String subject = st.nextToken();
+        for (int i = 0; i < 20; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            st.nextToken();
             double score = Double.parseDouble(st.nextToken());
             String grade = st.nextToken();
 
             if(grade.equals("P")) continue;
 
-            switch(grade) {
-                case "A+":
-                    sum += score * 4.5;
-                    break;
-                case "A0":
-                    sum += score * 4.0;
-                    break;
-                case "B+":
-                    sum += score * 3.5;
-                    break;
-                case "B0":
-                    sum += score * 3.0;
-                    break;
-                case "C+":
-                    sum += score * 2.5;
-                    break;
-                case "C0":
-                    sum += score * 2.0;
-                    break;
-                case "D+":
-                    sum += score * 1.5;
-                    break;
-                case "D0":
-                    sum += score;
-                    break;
-                default:
-                    sum += 0;
-                    break;
-            }
-
+            sum += score * gradeMap.get(grade);
             totalScore += score;
         }
 
         System.out.printf("%.6f", sum / totalScore);
+
     }
 }
